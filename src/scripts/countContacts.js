173da259a9ed131663693677ef07d49810a5960c
OLGA ZELENKO <olichkazelenko@gmail.com>
2025-09-08 08:49:19 +0100

    fix-bag

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -4,4 +4,4 @@ export const countContacts = async () => {
     const contacts = await getAllContacts();
     return contacts.length;
 };
-
+console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -3,4 +3,4 @@ export const getAllContacts = async () => {
 const contacts = await readContacts();
 return contacts || [];
 };
-
+console.log(await getAllContacts());
